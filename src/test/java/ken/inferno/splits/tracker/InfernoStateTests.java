c4e82b91d037180693b934223cbf1627555f26c5
Kendrick Choate <kendrick.choate@7factor.io>
2022-04-02 21:23:21 -0500

    Add first test to prove tests can pass and fail

---
+++ src/test/java/ken/inferno/splits/tracker/InfernoStateTests.java
@@ -0,0 +1,19 @@
+package ken.inferno.splits.tracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InfernoStateTests {
+
+    private InfernoState sut = new InfernoState();
+
+    @Test
+    public void test()
+    {
+        assertTrue(true);
+    }
+}
